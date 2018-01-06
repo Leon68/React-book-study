3deff89aea35dbfd7572d82aa0be182db47a73d2
Leon <wanliangwang@163.com>
2018-01-06 19:37:37 +0800

    显示代码块

--- src/components/commentApp/comment/Comment.js
+++ src/components/commentApp/comment/Comment.js
@@ -37,17 +37,32 @@ class Comment extends Component {
                 : `${Math.round(Math.max(duration,1))} 秒前`
         })
     }
+    _getProcessedContent(content) {
+        return content
+            .replace(/&/g, "&amp;")
+            .replace(/</g, "&lt;")
+            .replace(/>/g, "&gt;")
+            .replace(/"/g, "&quot;")
+            .replace(/'/g, "&#039;")
+            .replace(/`([\S\s]+?)`/g, '<code>$1</code>')
+    }
 
     handleDeleteComment(){
        if (this.props.onDeleteComment) {
            this.props.onDeleteComment(this.props.index)
        }
     }
+
     render() {
+        const {comment} = this.props
         return(
             <div className="comment-wrapper">
-                <span>{ this.props.comment.username }</span>:
-                <p>{ this.props.comment.content}</p>
+                <span>{ comment.username }</span>:
+                <p dangerouslySetInnerHTML={
+                    {
+                        __html: this._getProcessedContent(comment.content)
+                    }
+                } />
                 <span className='comment-createdtime'>
                     {this.state.timeString}
                 </span>
